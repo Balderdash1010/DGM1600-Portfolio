dfdcd5b7ae0f63ee522e1c24321bb689f0aede03
Tyler Petersen <71360850+Balderdash1010@users.noreply.github.com>
2022-11-11 21:40:30 -0700

    Star ship page

---
+++ starships/sw-starships.js
@@ -0,0 +1,52 @@
+import { starships } from "../data/starships.js";
+import { getLastNumber, removeChildren } from "../utils/index.js";
+
+const nav = document.querySelector(".nav");
+const navList = document.querySelector(".navlist");
+const shipView = document.querySelector(".shipViewer");
+
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".modal-close");
+const modalBackground = document.querySelector(".modal-background");
+const shipMessage = document.querySelector(".shipMessage");
+
+closeButton.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+modalBackground.addEventListener("click", () =>
+  modal.classList.toggle("is-active")
+);
+
+function populateNav() {
+  starships.forEach((starship) => {
+    let anchorWrap = document.createElement("a");
+    anchorWrap.href = "#";
+    let listItem = document.createElement("li");
+    listItem.textContent = starship.name;
+
+    anchorWrap.addEventListener("click", () => populateShipView(starship));
+
+    anchorWrap.appendChild(listItem);
+    navList.appendChild(anchorWrap);
+  });
+}
+
+populateNav();
+
+function populateShipView(shipData) {
+  removeChildren(shipView);
+  let shipImage = document.createElement("img");
+  let shipName = document.createElement("div");
+  shipName.className = 'shipName'
+  shipName.textContent = shipData.name
+  let shipNum = getLastNumber(shipData.url);
+  shipImage.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`;
+  shipImage.addEventListener("error", () => {
+    shipImage.hidden = true;
+    shipName.hidden = true
+    modal.classList.toggle("is-active");
+    shipMessage.textContent = `${shipData.name} is currently unavailable.`;
+  });
+  shipView.appendChild(shipImage);
+  shipView.appendChild(shipName);
+}
